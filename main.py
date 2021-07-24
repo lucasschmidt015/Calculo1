538f46a4cf3f1a2bb1e283e73076701a54442adb
lucasschmidt015 <lucas.schmidt015@gmail.com>
2021-07-23 21:39:03 -0300

    att

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
 import math
 
 def equacao(x):
-    if x <0:
-        for i in range(x, 0):
-            conta =i**3 +i**2 + math.sin(i) -2
-            print(conta)
-    else:
-        for i in range(0, x+1):
-            conta =i**3 +i**2 + math.sin(i) -2
-            print(conta)
+    conta = x**3 +x**2 + math.sin(x) -2
+    return conta
+
+def meio(a=None, b=None):
+    meio = (a - b)/2
+    return meio
 
 a = int(input())
 b = int(input())
-equacao(a)
-equacao(b)
+distancia = a - b
+contador = 0
+#while distancia > 0.1:
+Img1 = equacao(a)
+Img2 = equacao(b)
+pmeio = meio(a,b)
+ImgM = equacao(pmeio)
+b = pmeio
+if ImgM < 0 and Img1 > 0:
+    b = pmeio
+    print("A")
+    contador+=1
+elif ImgM < 0 and Img2 > 0:
+    a = pmeio
+    contador+=1
+    print("B")
+elif ImgM > 0 and Img1 < 0:
+    b = pmeio
+    print("C")
+    contador+=1
+elif ImgM > 0 and Img2 < 0:
+    a = pmeio
+    print("D")
+    contador+=1
+else:
+    print("não é possível afirmar que existe solução neste intervalo, tente outros dois números")
+    distancia = a - b
+
+ 
